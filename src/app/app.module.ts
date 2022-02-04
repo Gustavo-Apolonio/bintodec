1a130b765ef54eeaa09b2ee6867642a31f969427
caiodanielp <60896971+caiodanielp@users.noreply.github.com>
2022-02-04 17:24:39 -0300

    Bin to dec -- Started

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { BitComponent } from "./bit/bit.component"
+import { DecComponent } from './dec/dec.component';
+import { ContentComponent } from './content/content.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, HeaderComponent, DecComponent, BitComponent, ContentComponent],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
